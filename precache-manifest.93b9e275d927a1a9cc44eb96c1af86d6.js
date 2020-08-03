e1d545e4f4062d398d9dd73f1d15d94fd5b7509e
unknown <buenomiyuri@gmail.com>
2020-08-03 22:29:48 +0800

    Updates

---
+++ precache-manifest.93b9e275d927a1a9cc44eb96c1af86d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88bcfffdc0a6741325fe72f4e73066a",
+    "url": "/reactsocmedapp/index.html"
+  },
+  {
+    "revision": "f652da835544219b2399",
+    "url": "/reactsocmedapp/static/css/main.a158912f.chunk.css"
+  },
+  {
+    "revision": "e6a9698057751c57bf7e",
+    "url": "/reactsocmedapp/static/js/2.3bafa248.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactsocmedapp/static/js/2.3bafa248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f652da835544219b2399",
+    "url": "/reactsocmedapp/static/js/main.577173c3.chunk.js"
+  },
+  {
+    "revision": "4ddd9002839782024fdf",
+    "url": "/reactsocmedapp/static/js/runtime-main.a0f6a2ac.js"
+  }
+]);
